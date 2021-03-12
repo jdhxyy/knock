da08fbd8e1e30947abc14afe8b20bbed9592b753
jdhxyy <jdh821@163.com>
2021-03-12 09:58:05 +0800

    Defect:REQ008.002 Modify:zhouxin Desc:增加可变参数传入

--- knock.go
+++ knock.go
@@ -7,57 +7,57 @@ package knock
 
 // Resp 异步调用应答
 type Resp struct {
-    IsNeedResp bool
-    Bytes []uint8
-    Done  chan *Resp
+	IsNeedResp bool
+	Bytes      []uint8
+	Done       chan *Resp
 }
 
 // done 结果返回.框架内调用
 func (resp *Resp) done() {
-    select {
-    case resp.Done <- resp:
-    default:
-    }
+	select {
+	case resp.Done <- resp:
+	default:
+	}
 }
 
 // CallbackFunc 注册服务回调函数
 // 返回值是应答数据和应答标志.应答标志为false表示不需要应答
-type CallbackFunc func(req []uint8) ([]uint8, bool)
+type CallbackFunc func(req []uint8, params ...interface{}) ([]uint8, bool)
 
 var services = make(map[int]CallbackFunc)
 
 // Call 同步调用
 // 返回值是应答字节流和是否需要应答标志
-func Call(protocol uint16, cmd uint16, req []uint8) ([]uint8, bool) {
-    return callback(protocol, cmd, req)
+func Call(protocol uint16, cmd uint16, req []uint8, params ...interface{}) ([]uint8, bool) {
+	return callback(protocol, cmd, req, params...)
 }
 
 // CallAsync 异步调用
 // 返回值是应答字节流和是否需要应答标志
-func CallAsync(protocol uint16, cmd uint16, req []uint8) *Resp {
-    var resp Resp
-    resp.Done = make(chan *Resp, 10)
+func CallAsync(protocol uint16, cmd uint16, req []uint8, params ...interface{}) *Resp {
+	var resp Resp
+	resp.Done = make(chan *Resp, 10)
 
-    go func() {
-        resp.Bytes, resp.IsNeedResp = callback(protocol, cmd, req)
-        resp.done()
-    }()
+	go func() {
+		resp.Bytes, resp.IsNeedResp = callback(protocol, cmd, req, params...)
+		resp.done()
+	}()
 
-    return &resp
+	return &resp
 }
 
 // Register 注册服务回调函数
 func Register(protocol uint16, cmd uint16, callback CallbackFunc) {
-    rid := int(cmd) + (int(protocol) << 16)
-    services[rid] = callback
+	rid := int(cmd) + (int(protocol) << 16)
+	services[rid] = callback
 }
 
 // callback 回调命令字对应的函数
-func callback(protocol uint16, cmd uint16, req []uint8) ([]uint8, bool) {
-   rid := int(cmd) + (int(protocol) << 16)
-   v, ok := services[rid]
-   if ok == false {
-      return nil, false
-   }
-   return v(req)
+func callback(protocol uint16, cmd uint16, req []uint8, params ...interface{}) ([]uint8, bool) {
+	rid := int(cmd) + (int(protocol) << 16)
+	v, ok := services[rid]
+	if ok == false {
+		return nil, false
+	}
+	return v(req, params...)
 }
--- knock_test.go
+++ knock_test.go
@@ -1,77 +1,94 @@
 package knock
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
 func TestCase1(t *testing.T) {
-    Register(0, 1, callback1)
-    resp, ok := Call(0, 1, []uint8{2, 3, 4})
-    fmt.Println(ok, resp)
+	Register(0, 1, callback1)
+	resp, ok := Call(0, 1, []uint8{2, 3, 4})
+	fmt.Println(ok, resp)
 }
 
 // CallbackFunc 注册服务回调函数
 // 返回值是应答数据和应答标志.应答标志为false表示不需要应答
-func callback1(req []uint8) ([]uint8, bool) {
-    fmt.Println(req)
-    return []uint8{1, 2, 3}, true
+func callback1(req []uint8, params ...interface{}) ([]uint8, bool) {
+	fmt.Println(req)
+	return []uint8{1, 2, 3}, true
 }
 
 func TestCase2(t *testing.T) {
-    Register(0, 1, callback1)
-    resp, ok := Call(1, 1, []uint8{2, 3, 4})
-    fmt.Println(ok, resp)
+	Register(0, 1, callback1)
+	resp, ok := Call(1, 1, []uint8{2, 3, 4})
+	fmt.Println(ok, resp)
 }
 
 func TestCase3(t *testing.T) {
-    Register(0, 1, callback1)
-    resp := CallAsync(0, 1, []uint8{2, 3, 4})
-    <-resp.Done
-    fmt.Println(resp.IsNeedResp, resp.Bytes)
+	Register(0, 1, callback1)
+	resp := CallAsync(0, 1, []uint8{2, 3, 4})
+	<-resp.Done
+	fmt.Println(resp.IsNeedResp, resp.Bytes)
 }
 
 func TestCase4(t *testing.T) {
-    Register(0, 1, callback1)
-    resp := CallAsync(1, 1, []uint8{2, 3, 4})
-    <-resp.Done
-    fmt.Println(resp.IsNeedResp, resp.Bytes)
+	Register(0, 1, callback1)
+	resp := CallAsync(1, 1, []uint8{2, 3, 4})
+	<-resp.Done
+	fmt.Println(resp.IsNeedResp, resp.Bytes)
 }
 
 func TestCase5(t *testing.T) {
-    Register(0, 2, callback2)
-    for i := 0; i < 1000000; i++ {
-        resp := CallAsync(1, 1, []uint8{2, 3, 4})
-        <-resp.Done
-    }
+	Register(0, 2, callback2)
+	for i := 0; i < 1000000; i++ {
+		resp := CallAsync(1, 1, []uint8{2, 3, 4})
+		<-resp.Done
+	}
 }
 
 // CallbackFunc 注册服务回调函数
 // 返回值是应答数据和应答标志.应答标志为false表示不需要应答
-func callback2(req []uint8) ([]uint8, bool) {
-    for i := 0; i < 1000; i++ {
+func callback2(req []uint8, params ...interface{}) ([]uint8, bool) {
+	for i := 0; i < 1000; i++ {
 
-    }
-    return req, true
+	}
+	return req, true
 }
 
 func TestCase6(t *testing.T) {
-    for i := 0; i < 10000000; i++ {
-        callback2([]uint8{1, 2, 3})
-    }
+	for i := 0; i < 10000000; i++ {
+		callback2([]uint8{1, 2, 3})
+	}
 }
 
 func TestCase7(t *testing.T) {
-    Register(0, 2, callback2)
-    for i := 0; i < 10000000; i++ {
-        Call(0, 2, []uint8{2, 3, 4})
-    }
+	Register(0, 2, callback2)
+	for i := 0; i < 10000000; i++ {
+		Call(0, 2, []uint8{2, 3, 4})
+	}
 }
 
 func TestCase8(t *testing.T) {
-    Register(0, 2, callback2)
-    for i := 0; i < 10000000; i++ {
-        resp := CallAsync(0, 2, []uint8{2, 3, 4})
-        <-resp.Done
-    }
+	Register(0, 2, callback2)
+	for i := 0; i < 10000000; i++ {
+		resp := CallAsync(0, 2, []uint8{2, 3, 4})
+		<-resp.Done
+	}
+}
+
+func TestCase9(t *testing.T) {
+	Register(0, 3, callback3)
+
+	arr1 := []uint8{1, 2, 3}
+	arr2 := []int{4, 5, 6}
+	resp, ok := Call(0, 3, []uint8{2, 3, 4}, 2, arr1, arr2)
+	fmt.Println("resp", resp, ok)
+}
+
+// CallbackFunc 注册服务回调函数
+// 返回值是应答数据和应答标志.应答标志为false表示不需要应答
+func callback3(req []uint8, params ...interface{}) ([]uint8, bool) {
+	fmt.Println("req", req, params)
+	fmt.Println(len(params), params[1].([]uint8))
+	return req, true
 }
